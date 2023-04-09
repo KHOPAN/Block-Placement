22d5be5c1e19ac24be7275a1c5ebe6f2281f2866
KHOPAN <puthipnakchan@gmail.com>
2023-04-09 20:39:50 +0700

    Add /block and /block set commands

--- src/main/java/com/khopan/mc/mod/blockplacement/BlockPlacement.java
+++ src/main/java/com/khopan/mc/mod/blockplacement/BlockPlacement.java
@@ -3,7 +3,21 @@ package com.khopan.mc.mod.blockplacement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.command.argument.BlockStateArgumentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
 
 public class BlockPlacement implements ModInitializer {
 	public static final String MOD_NAME = "Block Placement";
@@ -11,8 +25,39 @@ public class BlockPlacement implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(BlockPlacement.MOD_NAME);
 
+	private BlockState blockState;
+
 	@Override
 	public void onInitialize() {
 		BlockPlacement.LOGGER.info("Initializing Block Placement Mod (Server)");
+		this.blockState = Blocks.STONE.getDefaultState();
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(CommandManager.literal("block").requires(source -> source.hasPermissionLevel(2)).executes(context -> {
+				ServerCommandSource source = context.getSource();
+				Entity entity = source.getEntity();
+
+				if(entity == null) {
+					throw new SimpleCommandExceptionType(Text.literal("Only entities can run this command")).create();
+				}
+
+				ServerWorld world = source.getWorld();
+
+				if(this.blockState == null) {
+					source.sendMessage(Text.literal("Internal Error: Field 'this.blockState' of class 'com.khopan.mc.mod.blockplacement.BlockPlacement' cannot be null").formatted(Formatting.RED));
+					source.sendMessage(Text.literal("Set to default block (minecraft:stone)"));
+					this.blockState = Blocks.STONE.getDefaultState();
+				}
+
+				BlockPos position = entity.getBlockPos();
+				world.setBlockState(position, this.blockState);
+				world.updateNeighbors(position, this.blockState.getBlock());
+				return Command.SINGLE_SUCCESS;
+			}).then(CommandManager.literal("set").then(CommandManager.argument("block", BlockStateArgumentType.blockState(registryAccess)).requires(source -> source.hasPermissionLevel(2)).executes(context -> {
+				this.blockState = BlockStateArgumentType.getBlockState(context, "block").getBlockState();
+				context.getSource().sendMessage(Text.literal("Successfully changed to '" + this.blockState.getBlock().getName().getString() + "'!"));
+				return Command.SINGLE_SUCCESS;
+			}))));
+		});
 	}
 }
--- src/main/java/com/khopan/mc/mod/blockplacement/BlockPlacementClient.java
+++ src/main/java/com/khopan/mc/mod/blockplacement/BlockPlacementClient.java
@@ -3,8 +3,59 @@ package com.khopan.mc.mod.blockplacement;
 import net.fabricmc.api.ClientModInitializer;
 
 public class BlockPlacementClient implements ClientModInitializer {
+	/*private static final int SYNC_COOLDOWN = 2;
+	private static final String PROPERTIES_FILE = "block-placement.properties";
+	private static final String DEFAULT_BLOCK = "minecraft:stone";
+	static String Block = null;
+	private static Properties Properties = null;
+	private static File ConfigFile = null;
+
+	static {
+		File configDirectory = FabricLoader.getInstance().getConfigDir().toFile();
+
+		if(!configDirectory.exists() && !configDirectory.mkdir()) {
+			BlockPlacement.LOGGER.warn("Could not create configuration directory '" + configDirectory.getAbsolutePath() + "'");
+		}
+
+		BlockPlacementClient.ConfigFile = new File(configDirectory, BlockPlacementClient.PROPERTIES_FILE);
+		BlockPlacementClient.Properties = new Properties();
+
+		if(BlockPlacementClient.ConfigFile.exists()) {
+			try {
+				FileInputStream stream = new FileInputStream(BlockPlacementClient.ConfigFile);
+				BlockPlacementClient.Properties.load(stream);
+				stream.close();
+			} catch(Throwable Errors) {
+				BlockPlacement.LOGGER.warn("Could not read '" + BlockPlacementClient.PROPERTIES_FILE + "' file");
+			}
+		}
+
+		BlockPlacementClient.Block = BlockPlacementClient.Properties.computeIfAbsent("block", key -> BlockPlacementClient.DEFAULT_BLOCK).toString();
+		BlockPlacementClient.sync();
+	}*/
+
+	//private int lastSync;
+
 	@Override
 	public void onInitializeClient() {
 		BlockPlacement.LOGGER.info("Initializing Block Placement Mod (Client)");
+		/*ServerWorldEvents.UNLOAD.register((server, world) -> {
+			int seconds = Calendar.getInstance().get(Calendar.SECOND);
+
+			if(this.lastSync == 0 || seconds - this.lastSync > BlockPlacementClient.SYNC_COOLDOWN) {
+				BlockPlacementClient.sync();
+				this.lastSync = seconds;
+			}			
+		});*/
 	}
+
+	/*private static void sync() {
+		try {
+			FileOutputStream stream = new FileOutputStream(BlockPlacementClient.ConfigFile);
+			BlockPlacementClient.Properties.store(stream, "Block Placement - Properties File");
+			stream.close();
+		} catch(Throwable Errors) {
+			BlockPlacement.LOGGER.warn("Could not save '" + BlockPlacementClient.PROPERTIES_FILE + "' file");
+		}
+	}*/
 }
